7556fa16ba4cfcad6785d491a6ea673f36ecd40a
riochuong <riochuong@gmail.com>
2015-06-03 03:19:59 -0700

    New Layout complete

--- NanoDegree/app/src/main/java/sd/chuongdao/nanodegree/NewMainActivity.java
+++ NanoDegree/app/src/main/java/sd/chuongdao/nanodegree/NewMainActivity.java
@@ -1,24 +1,23 @@
 package sd.chuongdao.nanodegree;
 
-import android.app.Activity;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class NewMainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_new_main, menu);
         return true;
     }
 
