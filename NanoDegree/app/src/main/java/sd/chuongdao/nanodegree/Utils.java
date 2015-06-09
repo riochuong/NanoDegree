8d549c3b100803ef8fee84704626c175e521d443
riochuong <riochuong@gmail.com>
2015-06-09 06:44:06 -0700

    fix projects with Nano feedbacks

--- NanoDegree/app/src/main/java/sd/chuongdao/nanodegree/Utils.java
+++ NanoDegree/app/src/main/java/sd/chuongdao/nanodegree/Utils.java
@@ -9,13 +9,20 @@ import android.widget.Toast;
  */
 public class Utils {
 
+
+    static Toast mToast = null;
+
     /**
      * Helper for displaying Toast
      * @param ctx
      * @param text
      */
     public static void makeToast(Context ctx, String text) {
-        Toast.makeText(ctx,text,Toast.LENGTH_LONG).show();
+        if (mToast != null)
+            mToast.cancel();
+        // save the reference to help reduce waiting time when showing toast
+        mToast = Toast.makeText(ctx,text,Toast.LENGTH_LONG);
+        mToast.show();
     }
 
 }
